113825262531eff3300cb497c39b03a3b16dd03b
Aaron Minnick <abminnick@gmail.com>
2021-11-16 14:31:29 -0800

    implement chained promises to display two colors on list, using multiple API calls
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import $ from "jQuery";
+import "./css/styles.css";
 import DinoService from "./dinoService";
 
 let p = 1;
@@ -8,25 +9,31 @@ let promise = DinoService.getFromApi(DinoService.dinoURL(p, w));
 promise.then(function(response) {
   const dino = JSON.parse(response);
   $('#dino-output').html(dinoFormat(dino));
-}, function(error) {
-  $('error-output').html(error);
-}); 
-
-let colorPromise = DinoService.getFromApi(DinoService.colorURL(140, 160, 200, 40, 20, 3));
-colorPromise.then(function(response) {
-  const color= JSON.parse(response);
-  console.log(color);
-  $('#bg').css('background-image', `url("${color.uri}")`);
-}, function(error) {
-  $('error-output').html(error);
+  return DinoService.getFromApi(DinoService.colorURL(140, 160, 200, 40, 5, 3));
+}).then(function(response) {
+  const color1= JSON.parse(response);
+  $('.color1').css('background-image', `url("${color1.uri}")`);
+  return DinoService.getFromApi(DinoService.colorURL(230, 60, 75, 40, 6, 7));
+}).then(function(response) {
+  const color2= JSON.parse(response);
+  $('.color2').css('background-image', `url("${color2.uri}")`);
+}).catch(function(error) {
+  $('error-output').html('something broke:' + error);
 }); 
 
 function dinoFormat(dinoJson) {
   let outputHtml = '';
   dinoJson.forEach(element => {
     outputHtml += '<ol>';
+    let colorPicker = true;
     element.forEach(subelement => {
-      outputHtml+=`<li>${subelement}</li>`;
+      if (colorPicker) {
+        outputHtml+=`<li class='color1'>${subelement}</li>`;
+        colorPicker = false;
+      } else {
+        outputHtml+=`<li class='color2'>${subelement}</li>`;
+        colorPicker = true;
+      }
     });
     outputHtml += '</ol>';
   });
